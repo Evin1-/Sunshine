49d3ce9b17aef6d1636e045d9598cc1d08e35342
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-12 18:39:35 -0500

    Add menu ForecastFragment

--- app/src/main/java/com/loopcupcakes/udacity/sunshine/fragments/ForecastFragment.java
+++ app/src/main/java/com/loopcupcakes/udacity/sunshine/fragments/ForecastFragment.java
@@ -3,6 +3,9 @@ package com.loopcupcakes.udacity.sunshine.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -41,6 +44,8 @@ public class ForecastFragment extends Fragment {
 
         listView.setAdapter(arrayAdapter);
 
+        setHasOptionsMenu(true);
+
         return rootView;
     }
 
@@ -48,5 +53,26 @@ public class ForecastFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         new FetchWeatherTask().execute();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
