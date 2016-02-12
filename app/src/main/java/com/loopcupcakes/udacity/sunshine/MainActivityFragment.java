4eb8ebf73e4209977aed0afd48aca236c8d4d362
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-12 16:40:55 -0500

    End lesson 1

--- app/src/main/java/com/loopcupcakes/udacity/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/loopcupcakes/udacity/sunshine/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +21,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Today - Sunny - 88/34");
+        arrayList.add("Tomorrow - Sunny - 88/34");
+        arrayList.add("Friday - Sunny - 88/34");
+        arrayList.add("Saturday - Sunny - 88/34");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_text, arrayList);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+        listView.setAdapter(arrayAdapter);
+
+        return rootView;
     }
 }
