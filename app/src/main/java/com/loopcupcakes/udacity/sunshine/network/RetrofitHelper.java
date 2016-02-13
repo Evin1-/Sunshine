652db1212eb709840b39cc2f0a4324db30379206
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-12 19:11:05 -0500

    Create RetrofitHelper in network package

---
+++ app/src/main/java/com/loopcupcakes/udacity/sunshine/network/RetrofitHelper.java
@@ -0,0 +1,40 @@
+package com.loopcupcakes.udacity.sunshine.network;
+
+import android.util.Log;
+
+import com.loopcupcakes.udacity.sunshine.entities.Result;
+import com.loopcupcakes.udacity.sunshine.utils.Constants;
+import com.loopcupcakes.udacity.sunshine.utils.Keys;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by evin on 2/12/16.
+ */
+public class RetrofitHelper {
+    private static final String TAG = "RetrofitHelperTAG_";
+
+    public Result getForecast(int postCode) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.WEATHER_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Weather weather = retrofit.create(Weather.class);
+
+
+        Call<Result> call = weather.forecast(postCode, "metric", 7, Keys.WEATHER_API_KEY);
+
+        Result result = null;
+        try {
+            result = call.execute().body();
+        } catch (IOException e) {
+            Log.e(TAG, "getForecast: ");
+        }
+        return result;
+    }
+}
--- app/src/main/java/com/loopcupcakes/udacity/sunshine/tasks/FetchWeatherTask.java
+++ app/src/main/java/com/loopcupcakes/udacity/sunshine/tasks/FetchWeatherTask.java
@@ -5,17 +5,10 @@ import android.util.Log;
 
 import com.loopcupcakes.udacity.sunshine.entities.Forecast;
 import com.loopcupcakes.udacity.sunshine.entities.Result;
-import com.loopcupcakes.udacity.sunshine.network.Weather;
-import com.loopcupcakes.udacity.sunshine.utils.Constants;
-import com.loopcupcakes.udacity.sunshine.utils.Keys;
+import com.loopcupcakes.udacity.sunshine.network.RetrofitHelper;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by evin on 2/12/16.
@@ -27,23 +20,11 @@ public class FetchWeatherTask extends AsyncTask<Integer, Void, List<Forecast>> {
     @Override
     protected List<Forecast> doInBackground(Integer... params) {
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Constants.WEATHER_BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        Weather weather = retrofit.create(Weather.class);
-
         int postCode = (params != null && params.length > 0) ? params[0] : 30339;
 
-        Call<Result> call = weather.forecast(postCode, "metric", 7, Keys.WEATHER_API_KEY);
+        RetrofitHelper retrofitHelper = new RetrofitHelper();
 
-        Result result = null;
-        try {
-            result = call.execute().body();
-        } catch (IOException e) {
-            Log.e(TAG, "doInBackground: " + e.toString());
-        }
+        Result result = retrofitHelper.getForecast(postCode);
 
         return (result != null) ? result.getForecast() : new ArrayList<Forecast>();
     }
