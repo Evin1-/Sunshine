13c1062909377281a993e5bbc6830605c907cfd5
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-12 20:16:04 -0500

    Format resultant date

--- app/src/main/java/com/loopcupcakes/udacity/sunshine/tasks/FetchWeatherTask.java
+++ app/src/main/java/com/loopcupcakes/udacity/sunshine/tasks/FetchWeatherTask.java
@@ -7,18 +7,21 @@ import com.loopcupcakes.udacity.sunshine.entities.Forecast;
 import com.loopcupcakes.udacity.sunshine.entities.Result;
 import com.loopcupcakes.udacity.sunshine.network.RetrofitHelper;
 
-import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by evin on 2/12/16.
  */
-public class FetchWeatherTask extends AsyncTask<Integer, Void, List<Forecast>> {
+public class FetchWeatherTask extends AsyncTask<Integer, Void, String[]> {
 
     private static final String TAG = "FetchWeatherTaskTAG_";
 
     @Override
-    protected List<Forecast> doInBackground(Integer... params) {
+    protected String[] doInBackground(Integer... params) {
 
         int postCode = (params != null && params.length > 0) ? params[0] : 30339;
 
@@ -26,16 +29,56 @@ public class FetchWeatherTask extends AsyncTask<Integer, Void, List<Forecast>> {
 
         Result result = retrofitHelper.getForecast(postCode);
 
-        return (result != null) ? result.getForecast() : new ArrayList<Forecast>();
+        return resultToString(result);
+    }
+
+    private String[] resultToString(Result result) {
+
+        final List<Forecast> forecastList = result.getForecast();
+        if (forecastList == null) {
+            return null;
+        }
+
+        final int size = forecastList.size();
+        String[] strings = new String[size];
+
+        for (int i = 0; i < size; i++) {
+            strings[i] = buildStringForecast(forecastList.get(i));
+        }
+
+        return strings;
+    }
+
+    private String buildStringForecast(Forecast forecast) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append(getDate(forecast.getDt()));
+        stringBuilder.append(" - ");
+        stringBuilder.append(forecast.getWeather().get(0).getMain());
+        stringBuilder.append(" - ");
+        stringBuilder.append(forecast.getTemp().getMax().intValue());
+        stringBuilder.append("/");
+        stringBuilder.append(forecast.getTemp().getMin().intValue());
+
+        return stringBuilder.toString();
+    }
+
+    private String getDate(long time) {
+        DateFormat simpleDateFormat = new SimpleDateFormat("c, MMM d", Locale.US);
+        Date netDate = (new Date(time * 1000));
+        return simpleDateFormat.format(netDate);
     }
 
     @Override
-    protected void onPostExecute(List<Forecast> forecastList) {
-        super.onPostExecute(forecastList);
-        Log.d(TAG, "onPostExecute: ");
+    protected void onPostExecute(String[] forecastArray) {
+        super.onPostExecute(forecastArray);
 
-        for (Forecast forecast : forecastList) {
-            Log.d(TAG, "onResponse: " + forecast.getTemp().getDay());
+        if (forecastArray == null) {
+            return;
+        }
+
+        for (String forecast : forecastArray) {
+            Log.d(TAG, "onResponse: " + forecast);
         }
 
     }
