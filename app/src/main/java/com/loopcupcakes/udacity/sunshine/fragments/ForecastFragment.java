cf80a5c6deb7b70ccb2e5ff8381420dbf90b234b
Evin1- <edwin.hdz.12.0@gmail.com>
2016-05-10 12:29:02 -0400

    Add projection Strings ForecastFragment

--- app/src/main/java/com/loopcupcakes/udacity/sunshine/fragments/ForecastFragment.java
+++ app/src/main/java/com/loopcupcakes/udacity/sunshine/fragments/ForecastFragment.java
@@ -45,6 +45,28 @@ public class ForecastFragment extends Fragment
     private ForecastAdapter mForecastAdapter;
     private ArrayList<String> mArrayList;
 
+    private static final String[] FORECAST_COLUMNS = {
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+            WeatherContract.LocationEntry.COLUMN_COORD_LONG
+    };
+
+    public static final int COL_WEATHER_ID = 0;
+    public static final int COL_WEATHER_DATE = 1;
+    public static final int COL_WEATHER_DESC = 2;
+    public static final int COL_WEATHER_MAX_TEMP = 3;
+    public static final int COL_WEATHER_MIN_TEMP = 4;
+    public static final int COL_LOCATION_SETTING = 5;
+    public static final int COL_WEATHER_CONDITION_ID = 6;
+    public static final int COL_COORD_LAT = 7;
+    public static final int COL_COORD_LONG = 8;
+
     public ForecastFragment() {
 
     }
@@ -181,7 +203,7 @@ public class ForecastFragment extends Fragment
 
         return new CursorLoader(getActivity().getApplicationContext(),
                 weatherForLocationUri,
-                null,
+                FORECAST_COLUMNS,
                 null,
                 null,
                 sortOrder);
