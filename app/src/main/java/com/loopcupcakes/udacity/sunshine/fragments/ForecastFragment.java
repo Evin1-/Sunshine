8afce225f3f815e5237a3cbbdf17ee09b0772f51
Evin1- <edwin.hdz.12.0@gmail.com>
2016-05-10 01:24:02 -0400

    Optimize imports

--- app/src/main/java/com/loopcupcakes/udacity/sunshine/fragments/ForecastFragment.java
+++ app/src/main/java/com/loopcupcakes/udacity/sunshine/fragments/ForecastFragment.java
@@ -7,8 +7,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.CursorAdapter;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
